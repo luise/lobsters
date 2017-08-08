1fc5245731fc7dfa44c516d9c51885ed1fc8aad2
Kevin Lin <kevin@quilt.io>
2017-08-08 09:42:52 -0700

    blueprint: Lint all files

--- lobsters-example.js
+++ lobsters-example.js
@@ -1,13 +1,13 @@
 // This is an example of how to use the Lobsters spec to deploy
 // Lobsters to a single Amazon instance.
 
-const {createDeployment, Machine} = require("@quilt/quilt");
-var lobsters = require("./lobsters.js")
+const { createDeployment, Machine } = require('@quilt/quilt');
+const lobsters = require('./lobsters.js');
 
-var deployment = createDeployment();
+const deployment = createDeployment();
 // To use this example, change this to your RSA public key.
-var sshPublicKey = "pubKey";
-var baseMachine = new Machine({provider: "Amazon", sshKeys: ["ssh-rsa " + sshPublicKey]});
+const sshPublicKey = 'pubKey';
+const baseMachine = new Machine({ provider: 'Amazon', sshKeys: [`ssh-rsa ${sshPublicKey}`] });
 
 deployment.deploy(baseMachine.asMaster());
 deployment.deploy(baseMachine.asWorker());
@@ -16,4 +16,4 @@ deployment.deploy(baseMachine.asWorker());
 // In the future, Quilt will have improved functionality for secrets.
 // Currently, SQL is only accessible from other containers in the cluster,
 // which limits the risk of this.
-lobsters.Deploy(deployment, "mysqlRootPassword");
+lobsters.deploy(deployment, 'mysqlRootPassword');
--- lobsters.js
+++ lobsters.js
@@ -1,36 +1,36 @@
 // Contains functionality to deploy Lobsters. See lobsters-example.js for
 // an example of how to use this file.
 
-const {Container, Service, publicInternet} = require("@quilt/quilt");
+const { Container, Service, publicInternet } = require('@quilt/quilt');
 
-exports.Deploy = function(deployment, sqlRootPassword) {
-    // Create a container for mysql.
-    var sqlContainer = new Container("mysql:5.6.32");
+exports.deploy = function deploy(deployment, sqlRootPassword) {
+  // Create a container for mysql.
+  const sqlContainer = new Container('mysql:5.6.32');
 
-    // Set SQL environment variables to initialize the root password and a
-    // lobsters user.
-    sqlContainer.setEnv("MYSQL_ROOT_PASSWORD", sqlRootPassword);
+  // Set SQL environment variables to initialize the root password and a
+  // lobsters user.
+  sqlContainer.setEnv('MYSQL_ROOT_PASSWORD', sqlRootPassword);
 
-    var sqlService = new Service("sql", [sqlContainer]);
+  const sqlService = new Service('sql', [sqlContainer]);
 
-    // Create a container for lobste.rs and configure it.
-    var lobstersContainer = new Container("quilt/lobsters");
+  // Create a container for lobste.rs and configure it.
+  const lobstersContainer = new Container('quilt/lobsters');
 
-    // Set the DATABASE_URL environment variable, which is how rails determines which database
-    // to connect to.
-    var sqlDatabaseUrl = "mysql2://root:" + sqlRootPassword + "@" +
-        sqlService.hostname() + ":3306/lobsters";
-    lobstersContainer.setEnv("DATABASE_URL", sqlDatabaseUrl);
+  // Set the DATABASE_URL environment variable, which is how rails determines which database
+  // to connect to.
+  const sqlDatabaseUrl = `mysql2://root:${sqlRootPassword}@${
+    sqlService.hostname()}:3306/lobsters`;
+  lobstersContainer.setEnv('DATABASE_URL', sqlDatabaseUrl);
 
-    var lobstersService = new Service("lobsters", [lobstersContainer]);
+  const lobstersService = new Service('lobsters', [lobstersContainer]);
 
-    // Allow inbound connections to SQL at 3306 (the default SQL port).
-    lobstersService.connect(3306, sqlService);
+  // Allow inbound connections to SQL at 3306 (the default SQL port).
+  lobstersService.connect(3306, sqlService);
 
-    // Allow inbound connections to the lobsters UI.
-    publicInternet.connect(3000, lobstersService);
+  // Allow inbound connections to the lobsters UI.
+  publicInternet.connect(3000, lobstersService);
 
-    // Deploy lobste.rs and mysql using the given deployment object.
-    deployment.deploy(sqlService);
-    deployment.deploy(lobstersService);
-}
+  // Deploy lobste.rs and mysql using the given deployment object.
+  deployment.deploy(sqlService);
+  deployment.deploy(lobstersService);
+};
