0bee2070ebb9464413a59203786f6d5408ee13a0
Kevin Lin <kevin@quilt.io>
2017-08-14 21:35:06 -0700

    Dockerfile: Fix typo

--- start-lobsters.sh
+++ start-lobsters.sh
@@ -13,6 +13,6 @@ echo "Lobsters::Application.config.secret_key_base = '`rake secret`'" >> \
 bin/rake db:seed
 
 # Run the rails server, and tell it to listen on 0.0.0.0 so that it will accept
-# incoming connections from all IPs associated with tme machine and not just
+# incoming connections from all IPs associated with the container and not just
 # localhost.
 rails server -b 0.0.0.0
