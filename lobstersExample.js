689df8b577270ea63b3bd2cab6ee069433c40d71
Luise Valentin <luise@kelda.io>
2017-10-27 14:10:50 -0700

    blueprint: Use Infrastructure constructor
    This commit replaces the deprecated `createDeployment` function with the
    new `Infrastructure` constructor.

--- lobstersExample.js
+++ lobstersExample.js
@@ -1,19 +1,17 @@
 // This is an example of how to use the Lobsters spec to deploy
 // Lobsters to a single Amazon instance.
 
-const { createDeployment, Machine } = require('kelda');
+const { Infrastructure, Machine } = require('kelda');
 const lobsters = require('./lobsters.js');
 
-const deployment = createDeployment();
 // To use this example, change this to your RSA public key.
 const sshPublicKey = 'pubKey';
 const baseMachine = new Machine({ provider: 'Amazon', sshKeys: [`ssh-rsa ${sshPublicKey}`] });
 
-deployment.deploy(baseMachine.asMaster());
-deployment.deploy(baseMachine.asWorker());
+const infra = new Infrastructure(baseMachine, baseMachine);
 
 // Deploy lobsters with "mysqlRootPassword" as the SQL root password.
 // In the future, Kelda will have improved functionality for secrets.
 // Currently, SQL is only accessible from other containers in the cluster,
 // which limits the risk of this.
-lobsters.deploy(deployment, 'mysqlRootPassword');
+lobsters.deploy(infra, 'mysqlRootPassword');
